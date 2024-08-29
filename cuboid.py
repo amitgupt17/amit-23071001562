b10ce88e3148c65dee521b7a3cb82919003062c7
Amit Gupta <138107458+amitgupt17@users.noreply.github.com>
2024-08-30 03:48:31 +0530

    Update cuboid.py

--- cuboid.py
+++ cuboid.py
@@ -3,3 +3,9 @@ width=float(input("Enter the width of cuboid = "))
 height=float(input("Enter the height of cuboid = "))
 area=2*(length*width+length*height+height*width)
 print("Area of cuboid is ",area)
+
+output
+Enter the length of cuboid = 5.5
+Enter the width of cuboid = 4.5
+Enter the height of cuboid = 6.5
+Area of cuboid is  179.5
