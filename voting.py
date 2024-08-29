315bc1f903f18dc29e012c0bb20c42e5c85c28b0
Amit Gupta <138107458+amitgupt17@users.noreply.github.com>
2024-08-30 03:10:48 +0530

    Update voting.py

--- voting.py
+++ voting.py
@@ -5,3 +5,12 @@ if age>=18:
     print("You can vote and Age is",age)
 elif age<18:
     print("You cannot vote after",age,"year for wait but you can vote now this year is",birth_year+18)
+
+output
+Enter the present year = 2024
+Enter the birth year = 1989
+You can vote and Age is 35
+again
+Enter the present year = 2024
+Enter the birth year = 2009
+You cannot vote after 15 year for wait but you can vote now this year is 2027
