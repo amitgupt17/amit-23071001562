6dbdea04d0afb86706130b812e38adf715aee5a7
Amit Gupta <138107458+amitgupt17@users.noreply.github.com>
2024-08-30 05:21:36 +0530

    Update complex.py

--- complex.py
+++ complex.py
@@ -6,3 +6,8 @@ elif C1.real<C2.real:
     print("The second complex number",C2,"has a greater real part")
 else:
     print("both complex numbers have equal real part")
+
+output
+Enter first complex number = 5+9j
+Enter second complex number = 2+7j
+The first complex number (5+9j) has a greater real part
