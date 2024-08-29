f7dcd5d1a431a333c03ff2ba82767a65138b32d2
Amit Gupta <138107458+amitgupt17@users.noreply.github.com>
2024-08-30 03:24:12 +0530

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -2,3 +2,8 @@ l=int(input("Enter the length = "))
 w=int(input("Enter the width = "))
 area=l*w
 print("Area of rectangle is ",area)
+
+output
+Enter the length = 6
+Enter the width = 8
+Area of rectangle is 48
