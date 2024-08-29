4968835e3ffa9c80438403904961cbe61d58e334
Amit Gupta <138107458+amitgupt17@users.noreply.github.com>
2024-08-30 04:58:57 +0530

    div

---
+++ divisible.py
@@ -0,0 +1,9 @@
+num=int(input("Enter the number = "))
+if num==0:
+    print("Number is zero and not divisible by both 2 and 8")
+elif num<0:
+    print(num,"is negative and can't be divisible by both 2 and 8")
+elif num%2==0 and num%8==0:
+    print(num,"is divisible by both 2 and 8")
+else:
+    print(num,"is not divisible by both 2 and 8")
