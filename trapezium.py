8c7c1118483e382d3fb393c1bc64e185db189a0b
Amit Gupta <138107458+amitgupt17@users.noreply.github.com>
2024-08-30 04:00:15 +0530

    trapezium

---
+++ trapezium.py
@@ -0,0 +1,5 @@
+a=float(input("Enter the length of the first parallel side(a) = "))
+b=float(input("Enter the length of the second parallel side(b) = "))
+h=float(input("Enter the height (h) = "))
+area = 0.5*(a+b)*h
+print("Area of trapezium is ",area)
