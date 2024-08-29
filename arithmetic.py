42ed73d2ab0cbe6fe7709ac7ee87a99795a3396f
Amit Gupta <138107458+amitgupt17@users.noreply.github.com>
2024-08-30 04:19:35 +0530

    arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+num1=float(input("Enter the first number = "))
+num2=float(input("Enter the second number = "))
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+floor_division=num1//num2
+modulus=num1%num2
+expotentiation=num1**num2
+print("Addition is ",addition)
+print("Subtraction is ",subtraction)
+print("Multiplication is ",multiplication)
+print("Division is ",division)
+print("Floor Divison is ",floor_division)
+print("Modulus is ",modulus)
+print("Expotentiation is ",expotentiation)
